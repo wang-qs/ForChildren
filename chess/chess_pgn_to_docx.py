f0d4e3b71035489e4fa6f73edfe01596d374de7e
Qiansheng Wang <qiansheng.wang@sap.com>
2021-01-18 20:29:12 +0800

    refactor the code

--- chess/chess_pgn_to_docx.py
+++ chess/chess_pgn_to_docx.py
@@ -7,6 +7,9 @@ from docx import Document
 from docx.shared import Inches, Cm
 from cairosvg import svg2png
 
+temp_svg_file = "chess_pgn_to_docx_temp_svg.svg"
+temp_png_file = "chess_pgn_to_docx_temp_svg.png"
+
 style_black_and_white = '''
 .square.light { fill: #cacaca; }
 .square.dark  { fill: #898989; }
@@ -44,16 +47,36 @@ def set_margin(section):
     section.left_margin = Inches(0.5)
     section.right_margin = Inches(0.5)
 
-def pgn_to_docx(workspace, pgn_filename, docx_filename,svg_style=None, flipped=False, show_initial_board=False):
-    temp_svg_file = "chess_pgn_to_docx_temp_svg.svg"
-    temp_png_file = "chess_pgn_to_docx_temp_svg.png"
-
-    document = Document()
-    document.add_heading(pgn_filename, 0)
+def update_docx_setting(document):
     sections = document.sections
     for section in sections:
         set_margin(section)
         set_number_of_columns(section,3)
+
+
+def write_list_number(document, list_line):
+    p = document.add_paragraph("", style='List Number')
+    p.add_run(list_line).bold = True
+
+def clean_temp_files():
+    os.remove(temp_svg_file)
+    os.remove(temp_png_file)
+
+def write_board_screenshot(document, board_in_svg):
+    with open(workspace + temp_svg_file, 'w') as content_file:
+        content_file.write(board_in_svg)  # create svg file.
+    with open(workspace + temp_svg_file, 'r') as content_file:
+        content = content_file.read()
+        svg2png(bytestring=content, write_to=temp_png_file)  # convert to png.
+    document.add_picture(temp_png_file, width=Inches(2))  # TODO: to show 4(row) * 3(column) boards, 2.2 is the max size.
+
+def write_title(document, title):
+    document.add_heading(title, 0)
+
+def pgn_to_docx(workspace, pgn_filename, docx_filename,svg_style=None, flipped=False, show_initial_board=False, first_move_white=True):
+    document = Document()
+    update_docx_setting(document)
+    write_title(document, pgn_filename)
 
     pgn = open(workspace + pgn_filename, encoding="utf-8")
     first_game = chess.pgn.read_game(pgn)
@@ -79,35 +102,23 @@ def pgn_to_docx(workspace, pgn_filename, docx_filename,svg_style=None, flipped=F
             print("Other variations: " + str(another_node.variations))
         print("")
 
-
     board = first_game.board()
     if show_initial_board:
-        current_svg = chess.svg.board(board=board, flipped=flipped, size=400, style=svg_style)
-        # create svg file.
-        f = open(workspace + temp_svg_file, "w")
-        f.write(current_svg)
-        f.close()
-        with open(workspace + temp_svg_file, 'r') as content_file:
-            content = content_file.read()
-            svg2png(bytestring=content, write_to=temp_png_file)
-        p = document.add_paragraph("", style='List Number')
-        p.add_run("Initial Board").bold = True
-        document.add_picture(temp_png_file, width=Inches(2))  # TODO: to show 4(row) * 3(column) boards, 2.2 is the max size.
+        write_list_number(document, "Initial Board")
+        board_in_svg = chess.svg.board(board=board, flipped=flipped, size=400, style=svg_style)
+        write_board_screenshot(document, board_in_svg)
 
     index = 1
     white = True
+    step = 0
     for move in first_game.mainline_moves():
+        step+=1
         board.push(move)
-        current_svg = chess.svg.board(board=board, lastmove=move, flipped=flipped, size=400, style=svg_style)
+        board_in_svg = chess.svg.board(board=board, lastmove=move, flipped=flipped, size=400, style=svg_style)
 
         turn = ""
         old_index = index
         old_white = white
-
-        # create svg file.
-        f = open(workspace + temp_svg_file, "w")
-        f.write(current_svg)
-        f.close()
 
         if white:
             turn = "White"
@@ -118,14 +129,17 @@ def pgn_to_docx(workspace, pgn_filename, docx_filename,svg_style=None, flipped=F
             index += 1
 
         # update the docx file.
-        if old_white:
-            write_step = san_map[old_index*2 -1]
-            blank_step = "-"
-            if old_index * 2 in san_map:
-                blank_step = san_map[old_index * 2]
-            #document.add_paragraph("## Step "+ str(old_index) +": " + write_step + ", " + blank_step , style='List Number')
-            p = document.add_paragraph("" , style='List Number')
-            p.add_run(write_step + ", " + blank_step).bold = True
+        if old_white or (step == 1 and (not first_move_white)):
+            # the branch that we should write a list number item. e.g. "1. e4, e5"
+            if step == 1 and (not first_move_white):
+                first_step = san_map[old_index*2 -1]
+                write_list_number(document, "... , " + first_step)
+            else:
+                write_step = san_map[old_index*2 -1]
+                blank_step = "-"
+                if old_index * 2 in san_map:
+                    blank_step = san_map[old_index * 2]
+                write_list_number(document, write_step + ", " + blank_step)
             comment = comment_map[old_index*2 -1]
         else:
             if old_index * 2 in comment_map:
@@ -133,10 +147,7 @@ def pgn_to_docx(workspace, pgn_filename, docx_filename,svg_style=None, flipped=F
             else:
                 comment = ""
 
-        with open(workspace + temp_svg_file, 'r') as content_file:
-            content = content_file.read()
-            svg2png(bytestring=content, write_to=temp_png_file)
-        document.add_picture(temp_png_file, width=Inches(2)) # TODO: to show 4(row) * 3(column) boards, 2.2 is the max size.
+        write_board_screenshot(document, board_in_svg)
         if comment:
             document.add_paragraph(turn + " Comment: " + comment )
 
@@ -145,12 +156,11 @@ def pgn_to_docx(workspace, pgn_filename, docx_filename,svg_style=None, flipped=F
     print(board)
     document.save(docx_filename)
 
-    os.remove(temp_svg_file)
-    os.remove(temp_png_file)
+    clean_temp_files()
 
 if __name__ == '__main__':
 
-    pgn_filename = "test.pgn"
+    pgn_filename = "test_doc.pgn"
     flipped = False
     show_initial_board = True
 
